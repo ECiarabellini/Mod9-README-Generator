daedd7d183e7c7260e7e69e31778a19845d44542
Emily Ciarabellini <136280931+ECiarabellini@users.noreply.github.com>
2024-01-25 18:08:05 -0800

    Add questions in inquirer format

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,61 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is your project name?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description explaining the what, why, and how of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.',
+    },
+    {
+        type: 'input',
+        name: 'screenshotLocation',
+        message: 'Provide the file location of a screenshot to include.',
+    },
+    {
+        type: 'input',
+        name: 'collaborators',
+        message: 'List your collaborators.',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Choose a license:',
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(writeToFile("README.md",data))
+}
 
 // Function call to initialize app
 init();
